4db01fb765d30e9a996b30f7063ca8371c3f0fbc
Duncan Haywood <duncanhaywoodacademic@gmail.com>
2020-08-13 14:51:12 -0700

    adds components section; begins to update app.js

--- src/App.js
+++ src/App.js
@@ -4,51 +4,14 @@ import './App.css';
 import axios from 'axios';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: ['Here is a default post value']
-    }
+  componentDidMount() {
+    const apiUrl = 'https://api.github.com/users/hacktivist123/repos';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
   }
-  componentDidMount(){
-    const apiUrl = 'http://dummy.restapiexample.com/api/v1/employees'// replace with 'https://api.qary.me/bot/?format=api';
-    axios({ method: 'get', url: `${apiUrl}` }).then(response => 
-    {
-    this.setState({
-      posts: 'ComponentsDidMount and posts are set and displayed' 
-      // replace with: response.data
-    })
-    /* throw new Error('componentDidMount is 
-    involved and runs all the way through!');*/
-  });
-  }
-  render(){
-    const {posts} = this.state
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Does this work?
-          </p>
-
-          <p>
-            Well, here are some Qary.API get calls, maybe?
-          </p>
-          <p>
-            {posts}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  render() {
+    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
   }
 }
 
